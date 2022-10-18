37a8ac5e36a705faaa71fd1efa2308e7bf155f77
elise bell <eliseraynahasawsomesauce@gmail.com>
2022-10-18 11:56:53 -0400

    began exercise

---
+++ Exercises/ES6Scripts/cheap_candy.js
@@ -0,0 +1,16 @@
+let products = [
+    {product: "Gummy Worms", price: 5.79},
+    {product: "Plain M&Ms", price: 2.89},
+    {product: "Peanut M&Ms", price: 2.89},
+    {product: "Swedish Fish", price: 3.79},
+    {product: "Skittles Original", price: 1.98},
+    {product: "Snickers Minis", price: 4.48},
+    {product: "Hershey Milk Chocolate Bar", price: 6.29},
+    {product: "Nerds", price: 3.48},
+    {product: "Sour Patch Kids", price: 5.77},
+    {product: "KitKat", price: 4.66},
+    // TODO: fill the array with 10 candies of various
+    // price ranges
+    ];
+
+    
