d6cf41ca5afe3f4d1be1c53dc893680f891dee24
elise bell <eliseraynahasawsomesauce@gmail.com>
2022-10-18 13:56:34 -0400

    improved display of swedish fish, added exercise

--- Exercises/ES6Scripts/cheap_candy.js
+++ Exercises/ES6Scripts/cheap_candy.js
@@ -26,7 +26,7 @@ console.log(`M&M candies in stock: `);
 console.log(mnmCandies);
 
 // Do we carry "Swedish Fish"?
-let foundCandy = products.find((product) => product.product == "Swedish Fish");
+let foundCandy = products.find((product) => product.product.includes("Swedish Fish"));
 console.log(" ");
 console.log("What we have in stock:");
-console.log(foundCandy);
+console.log(foundCandy.length != 0);
