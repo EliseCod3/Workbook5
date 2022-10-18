79b4ad53eaa65e2f5bf841c4c423816b59cfa3cf
elise bell <eliseraynahasawsomesauce@gmail.com>
2022-10-18 09:42:52 -0400

    added a reduce demo

---
+++ Demos/reduce.js
@@ -0,0 +1,13 @@
+let ticketPackage = [
+    {ticketType: "Senior", price: 49.99, numTickets: 1},
+    {ticketType: "Adult", price: 79.99, numTickets: 2},
+    {ticketType: "Child", price: 29.99, numTickets: 3}
+];
+
+function getTotalCost(currentTotal, ticket) {
+     return currentTotal + (ticket.price * ticket.numTickets);
+}
+
+    let sum = ticketPackage.reduce(getTotalCost, 0);
+    // sum contains 299.94
+    console.log(sum);
