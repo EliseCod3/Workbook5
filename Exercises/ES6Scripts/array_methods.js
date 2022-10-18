f6ce6b4d7bd1579d9d08a35fd8bb430e33e712d4
elise bell <eliseraynahasawsomesauce@gmail.com>
2022-10-18 11:11:06 -0400

    working on sorting item names

--- Exercises/ES6Scripts/array_methods.js
+++ Exercises/ES6Scripts/array_methods.js
@@ -8,9 +8,16 @@ let cart = [
     {item: "Apples", price: 0.66, quantity: 6}
 ];
 
+//return name of items
 function grabItemName(object) {
     return object.item;
 }
+
+function sortNames(a, b) {
+    if(a.item < b.item) return -1;
+    else if (a.item == b.item) return 0;
+    else return 1;
+};
 
 function displayName(itemName) {
     console.log(itemName);
@@ -19,3 +26,10 @@ function displayName(itemName) {
 let itemNames = cart.map(grabItemName);
 
 itemNames.forEach(displayName);
+console.log("-----------------------");
+//Total cost of cart
+function getSum(currentTotal, itemValue) {
+    return currentTotal + itemValue.price * itemValue.quantity;
+}
+let sum = cart.reduce(getSum, 0);
+console.log(`Total: ${sum}`);
