23e3bd199fea6af64c00796471d33ffd3a4cc900
elise bell <eliseraynahasawsomesauce@gmail.com>
2022-10-20 09:49:12 -0400

    adding option displaying

--- Exercises/DynamicImages/main.js
+++ Exercises/DynamicImages/main.js
@@ -6,15 +6,32 @@ let imageFiles = [
     {path: "./images/Kakashi.jpg/300/200", name: "Kakashi",}
 ];
 
-const imgList = document.getElementById("img-List");
+const characterList = document.getElementById("character-List");
 
 function loadCharacterList() {
     imageFiles.forEach((imageFile) => {
         let option = new Option(imageFile.name, imageFile.path);
-        imgList.appendChild(option);
-    })
+        characterList.appendChild(option);
+    });
+}
+
+function addNewImage() {
+    const selectedValue = characterList.value;
+
+    // const imagePath = null;
+    // imageFiles.forEach((imageFile) => {
+    //     if (imageFile.name === selectedValue) {
+    //         imagePath = imageFile.path;
+    //         return;
+    //     }
+    // });
+    let imageFile = imageFiles.find((f) => f.name === selectedValue);
+    console.log(imageFile.path);
 }
 
 window.onload = () => {
     loadCharacterList();
+
+    const addBtn = document.getElementById("add-btn");
+    addBtn.onclick = addNewImage;
 }
